cc0043d3d3f8a09ae748bd432b35284b4e12c80f
{yujlee} <yujlee242@gmail.com>
2021-08-07 00:44:09 +0900

    주석문 해제

--- Q1_Q20/Q15_Q20.js
+++ Q1_Q20/Q15_Q20.js
@@ -2,34 +2,34 @@
 
 //문제 15: 자기소개
 //이름을 입력하면 안녕하세요, 저는 ㅇㅇㅇ입니다. 라고 출력하기
-// {
-//   const name = prompt('이름 입력');
-//   console.log(`안녕하세요. 저는 ${name}입니다.`);
-// }
+{
+  const name = prompt('이름 입력');
+  console.log(`안녕하세요. 저는 ${name}입니다.`);
+}
 
 // //문제 16: 로꾸거
 // //문장이 입력되면 거꾸로 출력하는 프로그램 만들기
-// {
-//   const word = prompt('문장입력');
-//   const reverse = word.split('').reverse().join('');
-//   console.log(reverse);
+{
+  const word = prompt('문장입력');
+  const reverse = word.split('').reverse().join('');
+  console.log(reverse);
 
-//   //split() => 한글자씩 나눠 배열로 저장
-//   //reverse() => 배열을 뒤집음
-//   //join() => 다시 하나의 문자열80로 생성
-// }
+  //split() => 한글자씩 나눠 배열로 저장
+  //reverse() => 배열을 뒤집음
+  //join() => 다시 하나의 문자열80로 생성
+}
 
 // 문제 17 : 놀이기구 키 제한
 // 유주가 담당하는 놀이기구는 키가 150cm 이상만 탈 수 있다.
 // 입력으로 키가 주어졌을 때 키가 150이 넘으면 Yes, 틀리면 NO를 출력하는 프로그램
-// {
-//   const height = prompt('키를 입력하세요');
-//   if (height >= 150) {
-//     console.log('YES');
-//   } else {
-//     console.log('NO');
-//   }
-// }
+{
+  const height = prompt('키를 입력하세요');
+  if (height >= 150) {
+    console.log('YES');
+  } else {
+    console.log('NO');
+  }
+}
 
 // 문제 18: 평균 점수
 // 공백으로 구분하여 세 과목의 점수가 주어지면 전체 평균 점수를 구하는 프로그램 작성
