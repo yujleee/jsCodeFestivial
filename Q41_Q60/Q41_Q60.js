63b1f3474a19de9f8ff3cb3ea2bfff60a1eb89c1
{yujlee} <yujlee242@gmail.com>
2021-08-20 23:52:19 +0900

    47번까지 풀이

--- Q41_Q60/Q41_Q60.js
+++ Q41_Q60/Q41_Q60.js
@@ -65,3 +65,63 @@
 
   console.log(result);
 }
+
+//문제 44: 각 자리 수의 합
+//사용자가 입력한 양의 정수의 각 자리 수의 합을 구하는 프로그램
+//ex) 18234 = 1+8+2+3+4
+{
+  let num = prompt('숫자를 입력해주세요').split('');
+
+  let result = 0;
+  for (let i = 0; i < num.length; i++) {
+    result += parseInt(num[i]);
+  }
+
+  console.log(result);
+}
+
+//문제 45: getTime() 함수 사용하기
+//getTime()은 1970년 1월 1일 0시 0분 0초 이후부터 지금까지 흐른 시간을 ms 단위로 반환한다.
+//이를 이용하여 현재 연도를 출력
+{
+  let date = new Date();
+  const year = Math.floor(parseInt(date.getTime()) / 3.154e10) + 1970;
+
+  console.log(year);
+}
+
+//문제 46: 각 자리 수의 합2
+//1부터 20까지의 모든 숫자를 일렬로 놓고 모든 자릿수의 총 합을 구하세요.
+//ex) 10부터 15 => 101112131415 => 21
+{
+  let num = [];
+  for (let i = 1; i <= 20; i++) {
+    num[i] = i;
+  }
+
+  let result = 0;
+  num.forEach((n) => {
+    result += n;
+  });
+
+  console.log(result);
+}
+
+//문제 47: set 자료형의 응용
+//중복된 데이터를 삭제하여 실제 접수 명단이 몇 명인지 알고 싶다.
+//중복을 제거하여 실제 접수 인원을 출력
+{
+  const people = [
+    ('이호준', '01050442903'),
+    ('이호상', '01051442094'),
+    ('이준호', '01050342904'),
+    ('이호준', '01050442903'),
+    ('이준', '01050412904'),
+    ('이호', '01050443904'),
+    ('이호준', '01050442903'),
+  ];
+
+  //중복값이 있는 배열을 set 객체로 생성한다
+  const set = new Set(people);
+  console.log(`실제 접수인원: ${set.size}`);
+}
