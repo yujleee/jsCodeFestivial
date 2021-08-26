4f0db0935734bfa1dcc1be5f90730fc34a8fb92e
{yujlee} <yujlee242@gmail.com>
2021-08-26 23:35:17 +0900

    55번까지 풀이

--- Q41_Q60/Q41_Q60.js
+++ Q41_Q60/Q41_Q60.js
@@ -121,7 +121,7 @@
     ('이호준', '01050442903'),
   ];
 
-중복값이 있는 배열을 set 객체로 생성한다
+  //중복값이 있는 배열을 set 객체로 생성한다
   const set = new Set(people);
   console.log(`실제 접수인원: ${set.size}`);
 }
@@ -133,11 +133,11 @@
   let b = [];
   let s = '';
 
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] === a[i].toLowerCase()) {
-      b.push(a[i].toUpperCase());
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i].toLowerCase()) {
+      b.push(str[i].toUpperCase());
     } else {
-      b.push(a[i].toLowerCase());
+      b.push(str[i].toLowerCase());
     }
   }
 
@@ -183,75 +183,173 @@
   console.log(bubble(item));
 }
 
-
 //문제 51: merge sort (합병정렬)
-// 리스트의 길이가 0또는 1이면 정렬된 것으로 보고, 
+// 리스트의 길이가 0또는 1이면 정렬된 것으로 보고,
 //그 이외에는 반으로 잘라 정렬 후 다시 하나의 리스트로 합병.
 {
-  function mergesort(arr){
-    if(arr.length <= 1){
+  function mergesort(arr) {
+    if (arr.length <= 1) {
       return arr;
     }
 
-    const mid = Math.floor(arr.length /2); //리스트를 반으로 자르기 위한 기준
-    const left = arr.slice(0,mid);
+    const mid = Math.floor(arr.length / 2); //리스트를 반으로 자르기 위한 기준
+    const left = arr.slice(0, mid);
     const right = arr.slice(mid);
 
-    return merge(mergesort(left), mergesort(right)); 
-   }
+    return merge(mergesort(left), mergesort(right));
+  }
 
-   function merge(left, right){
-     let result = [];
+  function merge(left, right) {
+    let result = [];
 
-     while(left.length && right.length ){
-       if(left[0] < right[0]){
-         result.push(left.shift());
-       } else{
-         result.push(right.shift());
-       }
-     }
+    while (left.length && right.length) {
+      if (left[0] < right[0]) {
+        result.push(left.shift());
+      } else {
+        result.push(right.shift());
+      }
+    }
 
-
-     while(left.length){
+    while (left.length) {
       result.push(arr[i]);
-     }result.push(left.shift());
-     while(right.length){
-      l  result.push(right.shift());
-     }
+    }
+    result.push(left.shift());
+    while (right.length) {
+      result.push(right.shift());
+    }
 
-     return result;
-   }
+    return result;
+  }
 
-   const array = prompt('배열을 입력하세요.').split(' ').map(n => parseInt(n, 10));
-   console.log(mergeSort(array));
+  const array = prompt('배열을 입력하세요.')
+    .split(' ')
+    .map((n) => parseInt(n, 10));
+  console.log(mergeSort(array));
 }
 
-
-// 문제 52: 퀵정렬 
+// 문제 52: 퀵정렬
 {
-  function quickSort(arr){
-    if (arr.length <= 1){
+  function quickSort(arr) {
+    if (arr.length <= 1) {
       return arr;
-    } 
+    }
 
     const pivot = arr[0];
     const left = [];
     const right = [];
 
-    for(let i=1; i<arr.length; i++){
-      if(arr[i] < pivot){
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] < pivot) {
         left.push(arr[i]);
-      } else{
+      } else {
         right.push(arr[i]);
       }
     }
 
     return arr;
-
-    
-    
   }
-  const array = prompt('배열을 입력하세요.').split(' ').map(n => parseInt(n,10));
+  const array = prompt('배열을 입력하세요.')
+    .split(' ')
+    .map((n) => parseInt(n, 10));
 
   console.log(quickSort(array));
+}
+
+// 문제 53: 괄호 문자열
+// 괄호 모양이 바르게 구성된 문자열 => 바른 문자열. (())
+// 그렇지 않은 문자열 => 바르지 않은 문자열. ()())
+{
+  function bracketCheck(str) {
+    let cnt = 0;
+
+    for (let i = 0; i < str.length; i++) {
+      if (e[i] === '(') {
+        cnt++;
+      }
+
+      if (e[i] == ')') {
+        cnt--;
+      }
+    }
+
+    if (cnt !== 0) {
+      return false;
+    }
+
+    let bracket = [];
+    for (let i in str) {
+      if (str[i] == '(') {
+        bracket.push('(');
+      }
+
+      if (e[i] == ')') {
+        if (bracket.length == 0) {
+          return false;
+        }
+        bracket.pop();
+      }
+    }
+    return true;
+  }
+
+  const n = prompt('괄호를 바르게 구성하여 입력하세요').split('');
+
+  if (bracketCheck(n) == true) {
+    console.log('YES');
+  } else {
+    console.log('NO');
+  }
+}
+
+//문제 54: 연속되는 수
+//스탬프에 적힌 숫자가 공백으로 구분되어 주어지면 이 숫자가 연속수인지 아닌지
+//YES와 NO로 판별하는 프로그램
+{
+  const arr = prompt('숫자를 입력하세요').split(' ');
+
+  let cnt = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == i + 1) {
+      cnt++;
+    }
+  }
+  if (cnt == 5) {
+    console.log('YES');
+  } else {
+    console.log('NO');
+  }
+}
+
+//문제 55: 하노이의 탑
+//하노이의 탑은 A,B,C 3개의 기둥과 기둥에 꽂을 수 있는 N개의 원판으로 이루어져 있다.
+//또한 다음과 같은 규칙들을 만족해야 한다.
+//1. 처음에 모든 원판은 A기둥에 꽂혀 있다.
+//2. 모든 원판의 지름은 다르다.
+//3. 이 원반은 세 개의 기둥 중 하나에 반드시 꽂혀야 한다.
+//4. 작은 원반 위에 큰 원반을 놓을 수 없다.
+//5. 한 번에 하나의 원판만 옮길 수 있다.
+
+//위 규칙들을 만족하며 A기둥의 원반 N개를 모두 C원반으로 옮기고 싶다.
+//실행되어야 할 최소 원반 이동 횟수를 계산하는 프로그램
+{
+  const route = [];
+
+  function hanoi(num, start, end, mid) {
+    if (num === 1) {
+      route.push([start, end]);
+      return NaN;
+    }
+    //원반 n-1개를 경유기둥(c)로 이동
+    hanoi(num - 1, start, mid, end);
+
+    //가장 큰 원반은 목표기둥으로
+    route.push([start, end]);
+
+    //경유기둥와 시작기둥 바꿈
+    hanoi(num - 1, mid, end, start);
+  }
+
+  hanoi(3, 'a', 'b', 'c');
+  console.log(route);
+  console.log(route.length);
 }
