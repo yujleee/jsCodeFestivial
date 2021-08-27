49ccf44f13151c00575e46723870c7c389b290eb
{yujlee} <yujlee242@gmail.com>
2021-08-28 00:39:00 +0900

    58번까지 풀이

--- Q41_Q60/Q41_Q60.js
+++ Q41_Q60/Q41_Q60.js
@@ -353,3 +353,68 @@
   console.log(route);
   console.log(route.length);
 }
+
+//문제 56: 객체의 함수 응용
+//다음 객체가 주어졌을 때 한국의 면적과 가장 비슷한 국가와 그 차이를 출력
+
+//면적이 가장 비슷한 국가 => 차이가 가장 작은 국가
+{
+  nationWidth = {
+    korea: 220877,
+    Rusia: 17098242,
+    China: 9596961,
+    France: 543965,
+    Japan: 277915,
+    England: 242900,
+  };
+
+  const w = nationWidth['korea'];
+
+  delete nationWidth['korea'];
+
+  const entry = Object.entries(nationWidth);
+  /*
+    entries()
+    for..in문과 비슷하게 객체의 [key, value] 쌍의 배열을 반환 
+  */
+  const values = Object.values(nationWidth);
+  //values에 해당하는 값을 저장하는 변수
+
+  //gap에 최댓값 저장
+  let gap = Math.max.apply(null, values);
+  let item = [];
+
+  for (let i in entry) {
+    if (gap > Math.abs(entry[i][1] - w)) {
+      gap = Math.abs(entry[i][1] - w);
+      item = entry[i];
+    }
+  }
+
+  console.log(item[0], item[1] - w);
+}
+
+//문제 57: 1의 개수
+//0부터 1000까지 1의 개수를 세는 프로그램
+{
+  let cnt = 0;
+  for (let i = 0; i <= 1000; i++) {
+    let arr = (i + '').split('');
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j].includes('1')) {
+        cnt++;
+      }
+    }
+  }
+
+  console.log(`cnt: ${cnt}`);
+}
+
+//문제 58: 콤마찍기
+//숫자를 입력받고 천 단위로 콤마를 찍기.
+{
+  const num = parseInt(prompt('숫자를 입력하세요.'));
+
+  console.log(num.toLocaleString());
+  //toLocaleString() => 숫자를 특정 언어에 맞게 문자열로 반환해주는 함수.
+}
